e80d450dd40f808bd79fdd362763f544734ebf7f
Grant <grantwforsythe@gmail.com>
2021-08-07 15:01:23 +0000

    Add Week 9 lecture notes

--- week_9/src/frosh/application.py
+++ week_9/src/frosh/application.py
@@ -1,8 +1,30 @@
-from flask import Flask, render_template, request
+import os
+
+from cs50 import SQL
+# from flask_mail import Mail, Message
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+"""
+    There is a lot to configure when setting up emails.
 
-REGISTRAINTS = {}
+    To set up environment variables in Linux use the export command.
+
+    Ex:
+
+    export MAIL_DEFAULT_SENDER="grantwforsythe@gmail.com"
+
+"""
+# app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
+# app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+# app.config["MAIL_PORT"] = 587  # gmail
+# app.config["MAIL_SERVER"] = "smtp.gmail.com"
+# app.config["MAIL_USE_TLS"] = True
+# app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+# mail = Mail(app)
+
+
+db = SQL("sqlite:///froshims.db")
 
 SPORTS = [
         "Dodgeball",
@@ -31,7 +53,16 @@ def register():
     if sport not in SPORTS:
         return render_template("error.html", message="Invalid sport")
 
-    # store the form data
-    REGISTRAINTS[name] = sport
+    db.execute("INSERT INTO registrants (name, sport) VALUES (?, ?)", name, sport)
 
-    return render_template("registraints.html", name=name, sport=sport)
+    # message = Message("You are registered!")
+
+    return redirect("/registrants")
+
+
+@app.route("/registrants")
+def registrants():
+    # returns each row as a dictionary
+    registrants = db.execute("SELECT * FROM registrants")
+
+    return render_template("registraints.html", registrants=registrants)
