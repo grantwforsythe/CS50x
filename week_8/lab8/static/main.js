04bd3b8422c0e8ab0bb48c445e144ba724cc71bb
Grant Forsythe <grantwforsythe@gmail.com>
2021-08-10 00:55:57 -0400

    Finished Part 1

--- week_8/lab8/static/main.js
+++ week_8/lab8/static/main.js
@@ -1,14 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(){
     let button = document.querySelectorAll('button');
-    // TODO: reset the colors of none selected buttons
+    let answer = document.createElement('p');
     for (let i = 0; i < button.length; i++){
         button[i].addEventListener("click", function () {
             // this is an instance of an event object
-            if (this.id === "two"){
+            for (let j = 0; j < button.length; j++){
+                button[j].style.backgroundColor = "#d9edff";
+                answer.remove()
+            } 
+
+            if (this.id === "two") {
                 this.style.background = "green";
+                answer.textContent = "Correct!";
             } else {
                 this.style.background = "red";
+                answer.textContent = "Incorrect";
             }
+            this.append(answer);
         });
     }
 });
