8e42cae7b55165cd796050bac670696f824ca67c
Grant <grantwforsythe@gmail.com>
2021-08-12 22:07:16 +0000

    Change ORDER BY clause from ASC to DESC

--- psets/pset7/movies/11.sql
+++ psets/pset7/movies/11.sql
@@ -1,10 +1,10 @@
-SELECT title FROM movies
+SELECT movies.title FROM movies
 INNER JOIN
     stars ON movies.id = stars.movie_id
 INNER JOIN
     people ON stars.person_id = people.id
-INNER JOIN 
+INNER JOIN
     ratings ON movies.id = ratings.movie_id
 WHERE people.name = 'Chadwick Boseman'
-ORDER BY ratings.rating
+ORDER BY ratings.rating DESC
 LIMIT 5;
