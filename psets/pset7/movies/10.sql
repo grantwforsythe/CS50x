4dbc3dd9f4f786e0d01b7c58511653676076488d
Grant Forsythe <grantwforsythe@gmail.com>
2021-08-12 16:09:36 -0400

    Finish 10.sql

---
+++ psets/pset7/movies/10.sql
@@ -0,0 +1,9 @@
+SELECT people.name FROM people
+INNER JOIN
+    directors ON people.id = directors.person_id
+INNER JOIN
+    movies ON directors.movie_id = movies.id
+INNER JOIN
+    ratings ON movies.id = ratings.movie_id 
+WHERE ratings.rating>=9.0
+GROUP BY people.id;
