cecee3251033b90e5ba7ed86104e558c676da679
Grant Forsythe <grantwforsythe@gmail.com>
2021-08-12 16:33:42 -0400

    Finish 11.sql

---
+++ psets/pset7/movies/11.sql
@@ -0,0 +1,10 @@
+SELECT title FROM movies
+INNER JOIN
+    stars ON movies.id = stars.movie_id
+INNER JOIN
+    people ON stars.person_id = people.id
+INNER JOIN 
+    ratings ON movies.id = ratings.movie_id
+WHERE people.name = 'Chadwick Boseman'
+ORDER BY ratings.rating
+LIMIT 5;
